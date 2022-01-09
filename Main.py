358cd44160b3db07edd31128b329e0c65fcc9da0
anhemmay <69180385+anhemmay@users.noreply.github.com>
2022-01-09 22:42:24 +0700

    update main.py
    jdfisjd

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-print("hello")
+def tinh_tuoi_MSSV(namSinh):
+    return 2021 - int(namSinh)
+def get_email_MSSV(ten):
+    s = ten.split()
+    email =''
+    email += s[len(s)-1]
+    for i in range(len(s)-2 , -1, -1):
+        email += s[i][0]
+    email += '@gmail.com'
+    return email
+
+def tinh_thang_MSSV(thang , nam):
+    return (2021 - int(nam))*12 - int(thang )+ 12
